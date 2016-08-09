6b863f5f6538e6b6c46d78df194786373c102c50
jger <j.gerardis@gmail.com>
2016-08-09 14:19:33 +0000

    Not finished but email works

--- diavgeialert.py
+++ diavgeialert.py
@@ -7,6 +7,8 @@
 # Term and Subject that we looking for
 term = 'dipode2'
 subject = '%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF'
+me = 'noreply@myhomerpi.gr'
+you = 'j.gerardis@gmail.com'
 
 def check(term, subject):
   import urllib.request
@@ -20,10 +22,22 @@ def check(term, subject):
   data = json.loads(response.read().decode(encoding))
   return ([data['info']['actualSize'], data])
 
+def mail(me, you, text):
+  import smtplib
+  from email.mime.text import MIMEText
+  msg = MIMEText('Ok!' + str(text))
+  msg['Subject'] = 'You have a new alert!'
+  msg['From'] = me
+  msg['To'] = you
+  s = smtplib.SMTP('localhost')
+  s.sendmail(me, [you], msg.as_string())
+  s.quit()
+
+
 # Call the check function
 [chk, data] = check(term, subject)
 
 # Check if we had something 
 if int(chk)>0:
   # Check if this is somthing new
-  
+  mail(me, you, chk)
