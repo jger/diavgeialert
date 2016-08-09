121709f4fe062088a909dbe01f043b674f3d045a
jger <j.gerardis@gmail.com>
2016-08-09 11:22:37 +0000

    json

--- check.py
+++ check.py
@@ -1,15 +1,37 @@
 # This python program check if something new published on diavgeia
 #
 # the url is in the form of:
-# https://diavgeia.gov.gr/opendata/search?term=dipode2&from_date=2016-08-08&subject=%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF
+# https://diavgeia.gov.gr/opendata/search?term=KEYWORD&from_date=DATE&subject=SUBJECT
 # and return the number of result items 
 
 import urllib.request
+from datetime import date
+#from xml.dom.minidom import parse
+#import xml.dom.minidom
+import xml.etree.ElementTree as etree 
 
-url = 'https://diavgeia.gov.gr/opendata/search?term=dipode2&from_date=2016-08-08&subject=%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF'
+
+
+
+d = date.today()
+date = d.isoformat() 
+
+term = 'dipode2'
+subject = '%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF'
+url = 'https://diavgeia.gov.gr/opendata/search?term='+term+'&from_date='+date+'&subject='+subject
 
 
 with urllib.request.urlopen(url) as response:
-   html = response.read()
+   json = response.read()
 
-print (html)
+# Open XML document using minidom parser
+#DOMTree = xml.dom.minidom.parse(xml)
+#decisions = DOMTree.documentElement
+#info = decisions.getElementsByTagName("info")
+
+#print (info.childNodes[0].data)
+#print (info.childNodes[1].data)
+
+
+print(json)
+#tree = etree.ElementTree(etree.fromstring(xml))
