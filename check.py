8e9c11f49609565ecc042148a417e8348dbb01d9
jger <j.gerardis@gmail.com>
2016-08-09 11:51:03 +0000

    working example ok

--- check.py
+++ check.py
@@ -2,27 +2,22 @@
 #
 # the url is in the form of:
 # https://diavgeia.gov.gr/opendata/search?term=KEYWORD&from_date=DATE&subject=SUBJECT
-# and return the number of result items 
+# and return as result the number of items 
+# 
+# Author: Gerardis Ioannis
+# Date: 2016-08-09
 
 import urllib.request
 from datetime import date
-import xml.etree.ElementTree as etree 
 import json
-
-
 
 d = date.today()
 date = d.isoformat() 
-
-term = 'dipode2'
-subject = '%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF'
+term = 'dipode2' # example
+subject = '%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF' # example
 url = 'https://diavgeia.gov.gr/opendata/search?term='+term+'&from_date='+date+'&subject='+subject
-
-
-#with urllib.request.urlopen(url) as response:
-#   json = response.read()
-
-#print(json)
 response = urllib.request.urlopen(url)
 encoding = response.info().get_content_charset('utf8')
 data = json.loads(response.read().decode(encoding))
+
+print(data['info']['actualSize'])
