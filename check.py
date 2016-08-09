9137a8f3548dee39cf5adc2dfead92487467d086
jger <j.gerardis@gmail.com>
2016-08-09 09:06:41 +0000

    Ok

--- check.py
+++ check.py
@@ -1,11 +1,15 @@
 # This python program check if something new published on diavgeia
 #
-# the url is in the form of: https://diavgeia.gov.gr/search?query=q:%22KEY_WORD_HERE%22&page=0&sort=recent
+# the url is in the form of:
+# https://diavgeia.gov.gr/opendata/search?term=dipode2&from_date=2016-08-08&subject=%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF
 # and return the number of result items 
 
-
 import urllib.request
-with urllib.request.urlopen('http://python.org/') as response:
+
+url = 'https://diavgeia.gov.gr/opendata/search?term=dipode2&from_date=2016-08-08&subject=%CE%92%CE%B5%CF%81%CE%BF%CE%BB%CE%AF%CE%BD%CE%BF'
+
+
+with urllib.request.urlopen(url) as response:
    html = response.read()
 
-# print (html)
+print (html)
